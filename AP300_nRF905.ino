3557fb0afa20346c2f89cb2d0cb33d851f1954d8
stevstrong <stevestrong@online.de>
2015-07-13 11:42:21 +0200

    Update AP300_nRF905.ino
    cosmetics

--- AP300_nRF905.ino
+++ AP300_nRF905.ino
@@ -35,48 +35,48 @@ uint8_t plCounter;
 //////////////////////////////////////////////////////////////////////////
 void nRF905_Config(void)
 {
-  // Set control register 0 - CHANNEL
-  nRF905_setConfigReg0(0x76);
-  // config reg 1
-  nRF905_setConfigReg1(0x0E);
-  // config reg 2
-  nRF905_setConfigReg2(0x44);
-  // Set payload sizes
-  nRF905_setPayloadSizes(0x20);
-  // set TX and Rx addresses
-  nRF905_setTXAddress(buf1);
-  // set Rx addres
-  nRF905_setRXAddress(buf2);
-  // read config register 9 - just to be conform with data detected by sniffing
-  nRF905_getConfigReg(9);
-  // set config register 9
-  nRF905_setConfigReg9(0xDB);
-  // Clear DR by reading receive payload
-  nRF905_flushRecBuffer();
-  // Set interrupts
+	// Set control register 0 - CHANNEL
+	nRF905_setConfigReg0(0x76);
+	// config reg 1
+	nRF905_setConfigReg1(0x0E);
+	// config reg 2
+	nRF905_setConfigReg2(0x44);
+	// Set payload sizes
+	nRF905_setPayloadSizes(0x20);
+	// set TX and Rx addresses
+	nRF905_setTXAddress(buf1);
+	// set Rx addres
+	nRF905_setRXAddress(buf2);
+	// read config register 9 - just to be conform with data detected by sniffing
+	nRF905_getConfigReg(9);
+	// set config register 9
+	nRF905_setConfigReg9(0xDB);
+	// Clear DR by reading receive payload
+	nRF905_flushRecBuffer();
+	// Set interrupts
 //  REG_EXTERNAL_INT_CTL |= BIT_EXTERNAL_INT_CTL;
 //  nRF905_interrupt_on();
 }
 //////////////////////////////////////////////////////////////////////////
 void nRF905_Initialise()
 {
-  pinMode(PWR_MODE, OUTPUT);
-  digitalWrite(PWR_MODE, LOW);  // activate power down mode
-  pinMode(TRX_EN, OUTPUT);
-  digitalWrite(TRX_EN, LOW);  // activate standby mode
-  pinMode(TX_EN, OUTPUT);
-  pinMode(CSN, OUTPUT);
-  pinMode(CD, INPUT);
-  pinMode(DR, INPUT);
-  digitalWrite(CSN, HIGH);
+	pinMode(PWR_MODE, OUTPUT);
+	digitalWrite(PWR_MODE, LOW);  // activate power down mode
+	pinMode(TRX_EN, OUTPUT);
+	digitalWrite(TRX_EN, LOW);  // activate standby mode
+	pinMode(TX_EN, OUTPUT);
+	pinMode(CSN, OUTPUT);
+	pinMode(CD, INPUT);
+	pinMode(DR, INPUT);
+	digitalWrite(CSN, HIGH);
 #ifdef DBG_PIN
-  pinMode(DBG_PIN, OUTPUT);
+	pinMode(DBG_PIN, OUTPUT);
 #endif
 
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV2);
+	SPI.begin();
+	SPI.setClockDivider(SPI_CLOCK_DIV2);
 
-  nRF905_Config();
+	nRF905_Config();
 
 #if NRF905_INTERRUPTS
 	// Set interrupts
@@ -86,42 +86,41 @@ void nRF905_Initialise()
 
 Serial.println(F("nRF905 configured..."));
 StatusPins();
-// leave config mode
-  nRF905_powerUp();
+	// leave config mode
+	nRF905_powerUp();
 Serial.println(F("nRF905 powered up..."));
 StatusPins();
-  // Put into receive mode
-//  nRF905_receive();
+	// Put into receive mode
+	//nRF905_receive();
 }
 //////////////////////////////////////////////////////////////////
 void StatusPins(void)
 {
 return;
 /**/
-  Serial.print(F("pins: "));
-  Serial.print(digitalRead(CD));
-  Serial.print(",");
-  Serial.print(digitalRead(DR));
-  Serial.print(",");
-  Serial.print(digitalRead(PWR_MODE));
-  Serial.print(",");
-  Serial.print(digitalRead(TRX_EN));
-  Serial.print(",");
-  Serial.println(digitalRead(TX_EN));
+	Serial.print(F("pins: "));
+	Serial.print(digitalRead(CD));
+	Serial.print(",");
+	Serial.print(digitalRead(DR));
+	Serial.print(",");
+	Serial.print(digitalRead(PWR_MODE));
+	Serial.print(",");
+	Serial.print(digitalRead(TRX_EN));
+	Serial.print(",");
+	Serial.println(digitalRead(TX_EN));
 }
 ///////////////////////////////////////////////////////////////////
 void setup()
 {
 	Serial.begin(57600);
 	Serial.println(F("Client started"));
-  StatusPins();
+	//StatusPins();
 	// Start up
 	nRF905_Initialise();
-  StatusPins();
-  Serial.println(F("nRF905 initialised"));
-  plCounter = 0;
+	//StatusPins();
+	Serial.println(F("nRF905 initialised"));
+	plCounter = 0;
 }
-
 //////////////////////////////////////////////////////
 nRF905_radio_state_t nRF905_getStatus(void)
 {
@@ -135,7 +134,6 @@ nRF905_radio_state_t nRF905_getStatus(void)
 		else			return NRF905_RADIO_STATE_TX_END;
 	}
 }
-
 /////////////////////////////////////////////////////////////
 const char wr1[] PROGMEM = {0x90, 0x80, 0x03, 0x04, 0x00, 0x00, 0x0C};
 const char wr2[] PROGMEM = {0x90, 0x80, 0x03, 0x04, 0x0C, 0x00, 0x0C};
@@ -144,110 +142,110 @@ static byte recOK = 0;
 /////////////////////////////////////////////////////////////
 void loop()
 {
-  byte ret;
-
-  // Make data
-  strcpy_P((char*)payload, wr1);
-  counter++;
-  Serial.println(counter);
+	byte ret;
+	
+	// Make data
+	strcpy_P((char*)payload, wr1);
+	counter++;
+	Serial.println(counter);
 
 #ifdef DBG_PIN
-  digitalWrite(DBG_PIN, 1);
+	digitalWrite(DBG_PIN, 1);
 #endif
-  unsigned long startTime = millis();
+	unsigned long startTime = millis();
 /*	// Set address of device to send to
 	byte addr[] = TXADDR;
 	nRF905_setTXAddress(addr);
 */
 #if 0
 //// SET DATA
-  StatusPins();
-  Serial.println(F("setting data ..."));
-  // Set payload data
-  ret = nRF905_setData(payload, sizeof(wr1));
-  if (ret) Serial.println(F("Error by setting data!"));
+	StatusPins();
+	Serial.println(F("setting data ..."));
+	// Set payload data
+	ret = nRF905_setData(payload, sizeof(wr1));
+	if (ret) Serial.println(F("Error by setting data!"));
 
 //// SEND DATA
-  StatusPins();
-  Serial.println(F("sending data ..."));
-  // Send payload (send fails if other transmissions are going on, keep trying until success)
-  while(1) {
-    ret = nRF905_send();
-    if (ret==0) break;
-    else {
-      Serial.print(F("nRF905_send returned: "));
-      Serial.println(ret);
-      delay(100);
-    }
-    StatusPins();
-  };
+	StatusPins();
+	Serial.println(F("sending data ..."));
+	// Send payload (send fails if other transmissions are going on, keep trying until success)
+	while(1) {
+		ret = nRF905_send();
+		if (ret==0) break;
+		else {
+			Serial.print(F("nRF905_send returned: "));
+			Serial.println(ret);
+			delay(100);
+		}
+		StatusPins();
+	}
 #endif
 //// RECEIVE DATA
 #ifdef DBG_PIN
-  digitalWrite(DBG_PIN, 0);
-  StatusPins();
-  Serial.println(F("receiving data ..."));
+	digitalWrite(DBG_PIN, 0);
+	StatusPins();
+	Serial.println(F("receiving data ..."));
 #endif
-  // Put into receive mode
-  nRF905_receive();
+	// Put into receive mode
+	nRF905_receive();
 
-  // Make buffer for reply
+	// Make buffer for reply
 //  byte buffer[NRF905_MAX_PAYLOAD];
 #define buffer payload
 
 //// GETTING DATA
 #ifdef DBG_PIN
-  StatusPins();
-  Serial.println(F("getting data ..."));
+	StatusPins();
+	Serial.println(F("getting data ..."));
 #endif
-  // Wait for reply with timeout
-  unsigned long time0 = millis() + TIMEOUT;
-  while(1)
-  {
-    ret = nRF905_getData(&buffer[plCounter][0], NRF905_MAX_PAYLOAD);
-    if (ret==0) {plCounter++; recOK = true; break;}  // Got data
-    else {
+	// Wait for reply with timeout
+	unsigned long time0 = millis() + TIMEOUT;
+	while(1)
+	{
+		ret = nRF905_getData(&buffer[plCounter][0], NRF905_MAX_PAYLOAD);
+		if (ret==0) {plCounter++; recOK = true; break;}  // Got data
+		else {
 #ifdef DBG_PIN
-      Serial.print(F("getData returned: "));
-      Serial.println(ret);
+			Serial.print(F("getData returned: "));
+			Serial.println(ret);
 #endif
-    }
+		}
 //    StatusPins();
-    // check timeout
-    if ( millis() > time0 )  break;
-  }
+		// check timeout
+		if ( millis() > time0 )  break;
+	}
 
 //// EVALUATE DATA
 #ifdef DBG_PIN
-  StatusPins();
-  Serial.println(F("evaluating data..."));
+	StatusPins();
+	Serial.println(F("evaluating data..."));
 #endif
-  if ( ret==0 )
-  {  // data received. Do nothing, wait for time-out for display received data
+	if ( ret==0 )
+	{  // data received. Do nothing, wait for time-out for display received data
 #ifdef DBG_PIN
-    unsigned int totalTime = millis() - startTime;
-    Serial.print(F("Ping time: "));
-    Serial.print(totalTime);
-    Serial.println(F("ms"));
-    // Printout ping contents
-    Serial.print(F("Replay from AP300: "));
+		unsigned int totalTime = millis() - startTime;
+		Serial.print(F("Ping time: "));
+		Serial.print(totalTime);
+		Serial.println(F("ms"));
+		// Printout ping contents
+		Serial.print(F("Replay from AP300: "));
 #endif
-  }
-  else if (recOK) {
+	}
+	else if (recOK) {
 //    Serial.print(F("Replay from AP300: "));
-    for (uint8_t y = 0; y<plCounter; y++) {
-      for (byte i=0; i<NRF905_MAX_PAYLOAD; i++) {
-        ret = buffer[y][i];
-        if (ret<16)        Serial.print(0, HEX);
-        Serial.print(ret, HEX);
-      }
-      Serial.println();
-    }
-    recOK = false;
-    plCounter = 0;
-    Serial.println();
-  } else // time-out
-    Serial.println(F("time-out"));
+		for (uint8_t y = 0; y<plCounter; y++) {
+			for (byte i=0; i<NRF905_MAX_PAYLOAD; i++) {
+				ret = buffer[y][i];
+				if (ret<16)        Serial.print(0, HEX);
+				Serial.print(ret, HEX);
+			}
+			Serial.println();
+    	}
+		recOK = false;
+		plCounter = 0;
+		Serial.println();
+	} else // time-out
+		Serial.println(F("time-out"));
 /*
   if (counter>=100)
     while (1);  // stop here
